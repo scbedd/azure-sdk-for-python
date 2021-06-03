1d42059de6997db3eeffff04e357245a6c002dcf
scbedd <45376673+scbedd@users.noreply.github.com>
2021-06-03 16:15:41 -0700

    more scrubbing

--- eng/scripts/get_package_properties.py
+++ eng/scripts/get_package_properties.py
@@ -13,20 +13,23 @@ if __name__ == '__main__':
     parser.add_argument('-s', '--search_path', required=True, help='The scope of the search')
     args = parser.parse_args()
 
-    
+    seen_exception = 0
 
     for root, dirs, files in os.walk(args.search_path):
         if re.search(r"sdk[\\/][^\\/]+[\\/][^\\/]+$", root):
-                print (root)
-                for filename in files:
-                    if os.path.basename(filename) == "setup.py":
-                        #if os.path.basename(root) != 'azure-mgmt' and os.path.basename(root) != 'azure' and os.path.basename(root) != 'azure-storage' and os.path.basename(root) != 'tests':
-                        try:
-                            pkgName, version, is_new_sdk, setup_py_path = get_package_properties(root)
-                            print("{0} {1} {2} {3}".format(pkgName, version, is_new_sdk, setup_py_path))
-                        except:
-                            # Skip setup.py if the package cannot be parsed
-                            print ("error: " + root)
-                            print("did I hit this?")
+            for filename in files:
+                if os.path.basename(filename) == "setup.py":
+                    #if os.path.basename(root) != 'azure-mgmt' and os.path.basename(root) != 'azure' and os.path.basename(root) != 'azure-storage' and os.path.basename(root) != 'tests':
+                    try:
+                        print("entering {}".format(filename))
+                        pkgName, version, is_new_sdk, setup_py_path = get_package_properties(root)
+                        print("{0} {1} {2} {3}".format(pkgName, version, is_new_sdk, setup_py_path))
+                    except:
+                        # Skip setup.py if the package cannot be parsed
+                        print("excepting against {}".format(filename))
+                        seen_exception
+
+            
+                            
                         
                         
