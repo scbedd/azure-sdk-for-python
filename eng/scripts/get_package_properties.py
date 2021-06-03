a1661a69dc1938e8d40e42dba96097ee53299a04
scbedd <45376673+scbedd@users.noreply.github.com>
2021-06-03 16:13:50 -0700

    fix syntax

--- eng/scripts/get_package_properties.py
+++ eng/scripts/get_package_properties.py
@@ -2,6 +2,8 @@ import argparse
 import sys
 import glob
 import os
+import re
+import pdb
 
 sys.path.append(os.path.join('scripts', 'devops_tasks'))
 from common_tasks import get_package_properties
@@ -11,9 +13,20 @@ if __name__ == '__main__':
     parser.add_argument('-s', '--search_path', required=True, help='The scope of the search')
     args = parser.parse_args()
 
+    
+
     for root, dirs, files in os.walk(args.search_path):
-        for filename in files:
-            if os.path.basename(filename) == "setup.py":
-                if os.path.basename(root) != 'azure-mgmt' and os.path.basename(root) != 'azure' and os.path.basename(root) != 'azure-storage' and os.path.basename(root) != 'tests':
-                    pkgName, version, is_new_sdk, setup_py_path = get_package_properties(root)
-                    print("{0} {1} {2} {3}".format(pkgName, version, is_new_sdk, setup_py_path))
+        if re.search(r"sdk[\\/][^\\/]+[\\/][^\\/]+$", root):
+                print (root)
+                for filename in files:
+                    if os.path.basename(filename) == "setup.py":
+                        #if os.path.basename(root) != 'azure-mgmt' and os.path.basename(root) != 'azure' and os.path.basename(root) != 'azure-storage' and os.path.basename(root) != 'tests':
+                        try:
+                            pkgName, version, is_new_sdk, setup_py_path = get_package_properties(root)
+                            print("{0} {1} {2} {3}".format(pkgName, version, is_new_sdk, setup_py_path))
+                        except:
+                            # Skip setup.py if the package cannot be parsed
+                            print ("error: " + root)
+                            print("did I hit this?")
+                        
+                        
