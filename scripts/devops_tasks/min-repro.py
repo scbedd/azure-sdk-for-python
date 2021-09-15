5e3fc8fb9e1a2349b2506a86daae2fc89e4b172e
scbedd <45376673+scbedd@users.noreply.github.com>
2021-09-15 15:20:30 -0700

    added minimum repro files

--- scripts/devops_tasks/min-repro.py
+++ scripts/devops_tasks/min-repro.py
@@ -1,2 +1,9 @@
-from .common_tasks import parse_setup
+from common_tasks import parse_setup
 
+import os
+
+root_dir = os.path.abspath(os.path.join(os.path.abspath(__file__), "..", "..", ".."))
+target_package_for_parsing = os.path.join(root_dir, "sdk", "identity", "azure-identity")
+reqs = parse_setup(target_package_for_parsing)
+
+print(reqs)
